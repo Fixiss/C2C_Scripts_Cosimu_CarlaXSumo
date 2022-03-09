1eacdbd768374eda9534fdd67d3bd1e47e88310d
Fixiss <leo.segond@orange.fr>
2022-03-09 16:19:12 +0100

    feat: ajout du generateur de traffic pour la generation de NVE

--- script_carla/load_map.py
+++ script_carla/load_map.py
@@ -3,7 +3,7 @@ import sys
 args = sys.argv[1:]
 if(len(args)>=1):
     client = carla.Client('localhost', 2000)
-    client.set_timeout(100.0)
+    client.set_timeout(10.0)
     world = client.load_world(args[0])
 else:
     print("1 argument is required : arg -> map")
